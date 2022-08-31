a28c5746a3b0c25d088f10e2a73fdb05bbf272aa
vitorramos <ramos6382@gmail.com>
2022-08-31 16:54:21 -0300

    Entrega de exercicio 09

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaString02 =minhaString.trim()
+//const caracMinhaString = minhaString.length
+//const caracMinhaString02 = minhaString02.length
+
+console.log(minhaString, minhaString.length)
+console.log(minhaString02, minhaString02.length)
+ const minhaString03 = minhaString02.replace("________","sticioso")
+ console.log(minhaString03)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair`
+console.log(frase)
+const frase02 = frase.replace("verde","rosa").replace("azul","laranja")
+console.log(frase02)
+
+const res01=frase02.includes("verde")
+const res02=frase02.includes("laranja")
+const somafrase = res01 && res02
+console.log(somafrase)
